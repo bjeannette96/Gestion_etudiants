8a5ab72b3429aab3062ae54e3d949ce3f415d6fa
edgboy <edgardmartial@gmail.com>
2025-10-28 11:54:09 +0100

    finish step 2

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
-etudiants = [
-    {
-        "id": 1,
-        "nom": "Alice Dupont",
-        "email": "alice@exemple.com",
-        "notes": [15.5, 12.0, 18.0],
-    },
-    {
-        "id": 2,
-        "nom": "Bob Martin",
-        "email": "bob@exemple.com",
-        "notes": [10.0, 14.5, 16.0],
-    },
-]
+import json
+
+with open('student_list.json', 'r') as fichier_json:
+    students = json.load(fichier_json)
+    print(students)
 
 
-def calculer_moyenne(notes):
+
+
+def calculer_moyenne(grades):
     "This function returns the average of a list"
-    if not notes:
+    if not grades:
         return 0
-    return float(sum(notes) / len(notes))
+    return float(sum(grades) / len(grades))
 
 
-def affichez(students):
+def afficher():
     "This function returns the list of students with their average"
     try:
         if not students:
             return None
         for dictionnaire in students:
-            nom = dictionnaire["nom"]
-            mean = calculer_moyenne(dictionnaire["notes"])
-            print(f"L'étudiant {nom} a pour moyenne : {mean}")
+            name = dictionnaire["name"]
+            mean = calculer_moyenne(dictionnaire["grades"])
+            print(f"{name}'smean is: {mean}")
     except:
         print("An exception occurred")
 
 
-# affichez(etudiants)
+# affichez(students)
 
 
 def add_student():
     student = {}
-    student["id"] = len(etudiants) + 1
+    student["id"] = len(students) + 1
     print("Veuillez entrer les données de l'étudiant")
-    student["name"] = input("Entrer le nom complet de l'étudiant:")
-    student["email"] = input("Entrer le mail:")
-    student["notes"] = []
+    student["name"] = input("Entrer le name complet de l'étudiant:")
+    student["mail"] = input("Entrer le mail:")
+    student["grades"] = []
 
     y = "Y"
     while y == "Y":
@@ -51,9 +44,9 @@ def add_student():
         try:
             note = float(note)
             Invalid_Number = note < 0 or note > 20
-            student["notes"].append(note)
+            student["grades"].append(note)
             y = input("Ajouter une note? Y/N : ").upper()
-            etudiants.append(student)
+            students.append(student)
         except Invalid_Number:
             print("Wrong input, please try again.")
         except:
@@ -62,12 +55,25 @@ def add_student():
             return print("Thank you!\n")
 
 
-# def search_by_ID(id):
-#     try:
+def search_by_ID():
+    try:
+        id = input("Entrer l'id de l'étudiant : ")
         
-#     except:
-#         print("Wrong input, please try again.")
+        for student in students:
+            right_id=student.get("id")
+            if right_id == int(id):
+                return print(f"L'étudiant trouvé : {student}")
+        print("Student not found !!!")
+    except:
+        print("Wrong input, please try again.")
 
+def save_students_list():
+    try:
+        with open('student_list.json', 'w') as fichier_json:
+            json.dump(students, fichier_json)
+            print("Liste des étudiants sauvegardée avec succès.")
+    except:
+        print("Erreur lors de la sauvegarde de la liste des étudiants.")
 
 
 def menu(choice):
@@ -76,21 +82,21 @@ def menu(choice):
         case 1:
             add_student()
         case 2:
-            affichez(etudiants)
+            afficher()
         case 3:
-            search_by_ID(id)
+            search_by_ID()
         case 4:
-            print("Thursday")
+            save_students_list()
         case _:
-            print("No match")
+            exit()
 
 
 def display_menu():
-    print("****************MENU*************\n")
+    print("\n ****************MENU*************\n")
     print('1 - Ajouter un étudiant')
     print('2 - Afficher tous les étudiants')
     print('3 - Recherchez un étudiant')
-    print('4 - Sauvegarder et quitter \n')
+    print('4 - Sauvegarder')
     return
 
 
@@ -106,11 +112,11 @@ def main():
             y = input("Continuer? Y/N : ").upper()
         except Invalid_options:
             print("Wrong choice, please try again.")
+            return
         except:
             print("Wrong input, please try again.")
-        finally:
-            return print("Thank you! Good Bye ... \n")
+    
+    return print("Thank you! Good Bye ... \n")
         
-    return
 
 main()
